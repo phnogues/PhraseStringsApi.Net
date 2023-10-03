4e31ddc3ef006a1b60f40c6274c17b427335d3e9
Vitalii.Klymchuk <007kvv@gmail.com>
2023-10-03 13:45:07 +0300

    adds rate limit info

---
+++ PhraseStrings.Api/Model/LimitVariables.cs
@@ -0,0 +1,17 @@
+﻿namespace PhraseStrings.Api.Model
+{
+    internal static class LimitVariables
+    {
+        public static object NumberOfAllowedRequestsLock { get; } = new object();
+
+        public static object NumberOfRemainingRequestsLock { get; } = new object();
+
+        public static object DateTimeOfLimitResetingLock { get; } = new object();
+
+        public static int? NumberOfAllowedRequests { get; set; }
+
+        public static int? NumberOfRemainingRequests { get; set; }
+
+        public static DateTimeOffset? DateTimeOfLimitReseting { get; set; }
+    }
+}
--- PhraseStrings.Api/Services/BaseService.cs
+++ PhraseStrings.Api/Services/BaseService.cs
@@ -8,6 +8,10 @@ namespace PhraseStrings.Api.Services;
 
 internal abstract class BaseService
 {
+    private const string XRateLimitLimitHeader = "X-Rate-Limit-Limit";
+    private const string XRateLimitRemainingHeader = "X-Rate-Limit-Remaining";
+    private const string XRateLimitResetHeader = "X-Rate-Limit-Reset";    
+
     protected HttpClient HttpClient;
     protected JsonSerializerOptions JsonSerializerOptions;
 
@@ -26,6 +30,8 @@ internal abstract class BaseService
     {
         var result = await HttpClient.GetAsync(requestUri);
 
+        ReadLimitVariables(result);
+
         if (result.StatusCode == System.Net.HttpStatusCode.NotFound)
             return default;
 
@@ -38,6 +44,7 @@ internal abstract class BaseService
     {
         var result = await HttpClient.GetAsync(requestUri);
 
+        ReadLimitVariables(result);
         HandleError(result);
 
         return await HandleResult<TResult>(result);
@@ -61,6 +68,7 @@ internal abstract class BaseService
             result = await HttpClient.SendAsync(requestMessage);
         }
 
+        ReadLimitVariables(result);
         HandleError(result);
 
         return await HandleResult<TResult>(result);
@@ -71,6 +79,7 @@ internal abstract class BaseService
         var data = request.ToPhraseDatasKeyValue();
         var result = await HttpClient.PatchAsync(requestUri, new FormUrlEncodedContent(data));
 
+        ReadLimitVariables(result);
         HandleError(result);
 
         return await HandleResult<TResult>(result);
@@ -106,6 +115,47 @@ internal abstract class BaseService
 
             default:
                 throw new Exception($"An error occured: {responseMessage.StatusCode}");
+        }
+    }
+
+    private void ReadLimitVariables(HttpResponseMessage response)
+    {
+        if (response is null)
+            return;
+
+        var headers = response.Headers;
+
+        if (headers.Contains(XRateLimitLimitHeader) && headers.TryGetValues(XRateLimitLimitHeader, out IEnumerable<string>? xRateLimitLimitHeaderValues))
+        {
+            if (int.TryParse(xRateLimitLimitHeaderValues?.FirstOrDefault(), out int result))
+            {
+                lock (LimitVariables.NumberOfAllowedRequestsLock)
+                {
+                    LimitVariables.NumberOfAllowedRequests = result;
+                }
+            }
+        }
+
+        if (headers.Contains(XRateLimitRemainingHeader) && headers.TryGetValues(XRateLimitRemainingHeader, out IEnumerable<string>? xRateLimitRemainingHeaderValues))
+        {
+            if (int.TryParse(xRateLimitRemainingHeaderValues?.FirstOrDefault(), out int result))
+            {
+                lock (LimitVariables.NumberOfRemainingRequestsLock)
+                {
+                    LimitVariables.NumberOfRemainingRequests = result;
+                }
+            }
+        }
+
+        if (headers.Contains(XRateLimitResetHeader) && headers.TryGetValues(XRateLimitResetHeader, out IEnumerable<string>? values))
+        {
+            if (long.TryParse(values?.FirstOrDefault(), out long result))
+            {
+                lock (LimitVariables.DateTimeOfLimitResetingLock)
+                {
+                    LimitVariables.DateTimeOfLimitReseting = DateTimeOffset.FromUnixTimeSeconds(result);
+                }
+            }
         }
     }
 }
